c8c6a9606b5d4eeff6fb2a09bfdcdcfd3892b35d
xxhirotakaxx <m.hirotaka0628@me.com>
2018-11-14 20:33:33 +0900

    Initial commit

---
+++ app/src/main/java/com/me/hirotaka/taskapp/Task.java
@@ -0,0 +1,57 @@
+package com.me.hirotaka.taskapp;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+
+public class Task extends RealmObject implements Serializable {
+    private String title;
+    private String contents;
+    private String category;
+    private Date date;
+
+    @PrimaryKey
+    private int id;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+
+    public  void setContents(String contents) {
+        this.contents = contents;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
